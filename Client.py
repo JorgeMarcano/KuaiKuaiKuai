bea373fd3b696ce615fe26dfde0f09f21b717a1c
Hveen8 <harry.vanderveen8@gmail.com>
2024-08-10 00:02:52 -0400

    Updated thread logic and logging

--- Client.py
+++ Client.py
@@ -1,44 +1,138 @@
-import socket
 import ipaddress
+import logging
+import socket
+import threading
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+####################
+# Global Variables #
+####################
+
+LOCALHOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 HOST_PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+BUFFER_SIZE = 1024
 
 HANDSHAKE = str.encode("aok")
 
+logging.basicConfig(level=logging.DEBUG)
+
+####################
+# Class Definition #
+####################
+
 class Client:
-    def __init__(self, host, port):
+    def __init__(self, host, port, callback = None):
         try:
+            # Create members for socket, read data buffer, and callback
             self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.__socket.connect((host, port))
+            self.__buffer = [0] * BUFFER_SIZE
+            self.__buffer_full = False
+            self.__callback = callback
+            self.__thread = None
             
-            print(f"Connected to {self.__socket.getsockname()[0]}")
+            self.__socket.connect((host, port))
+            logging.info(f"Connected to {self.__socket.getsockname()[0]}")
+
+            # Listen for server activity on another thread
+            self.__thread = threading.Thread(target=self.daemon, daemon=True)
+            self.__thread.start()
+        except ConnectionRefusedError as e:
+            logging.error(f"Machine at {host} refused connecton")
+            self.__socket = None
         except Exception as e:
             raise e
 
     def close(self):
-        self.__socket.close()
+        try:
+            self.__socket.shutdown(socket.SHUT_WR)
+        except Exception:
+            # Ignore error if the socket is already closed
+            pass
+        finally:
+            self.__socket.close()
+            self.__socket = None
+            logging.info("Client socket has been closed")
+            
+    def daemon(self):
+        try:
+            while True:
+                # Listen for moves from server
+                self.__buffer = self.__socket.recv(BUFFER_SIZE)
+                self.__buffer_full = True
+
+                if not self.__buffer:
+                    break
+
+                logging.info(f"Received data: {self.__buffer.decode()}")
+
+                # The callback is called whenever the client receives data
+                if self.__callback:
+                    self.__callback(self)
+
+        except ConnectionResetError as e:
+            logging.warning(f"The server forcefully disconnected")
+
+        # Server has closed the connection, close client-side connection
+        self.close()
+        logging.info(f"Closing connection due to server disconnect")
 
     def send(self, message):
         self.__socket.sendall(message)
-        data = self.__socket.recv(1024)
 
-        return data
+    def is_connected(self):
+        return self.__socket and self.__socket.fileno() != -1
+    
+    def is_buffer_full(self):
+        return self.__buffer_full == True
 
-# Get Server IP
-host = None
-while host == None:  
-    try:
-        host = input("Enter server IP:")
-        ipaddress.ip_address(host)
-    except ValueError:
-        print('address/netmask is invalid: %s' % host)
-        host = None
-    except:
-        print('Usage : %s  ip' % host)
-        host = None
+    def read_buffer(self):
+        self.__buffer_full = False
+        return self.__buffer.decode()
 
+####################
+# Helper Functions #
+####################
 
-client = Client(host, HOST_PORT)
+# Get Server IP using console
+def get_host_IP_from_user():
+    host = None
+    while host == None:  
+        try:
+            host = input("Enter server IP:")
+            ipaddress.ip_address(host)
+        except ValueError:
+            logging.error('address/netmask is invalid: %s' % host)
+            host = None
+        except:
+            logging.error('Usage : %s  ip' % host)
+            host = None
+    
+    return host
 
-print(client.send(str.encode("Test")).decode())
+# Simple callback that prints the data received from the server in reverse
+def sample_callback(client):
+    print(f"Callback Output: {client.read_buffer()[::-1]}")
+
+#################
+# Sample Client #
+#################
+
+# Establish connection with server
+client = None
+while True:
+    host = get_host_IP_from_user()
+    client = Client(host, HOST_PORT, sample_callback)
+
+    if client.is_connected():
+        break
+
+# Run Client
+logging.info(f"Type messages and press enter to send them to the server (type \"end\" to close client)")
+while True:
+    user_input = input()
+    if user_input == "end" or not client.is_connected():
+        break
+
+    client.send(str.encode(user_input))
+
+client.close()
--- Host.py
+++ Host.py
@@ -1,11 +1,17 @@
+import logging
 import socket
 import threading
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+LOCALHOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+HOST = socket.gethostbyname(socket.gethostname())
 HOST_PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+BUFFER_SIZE = 1024
 
 PLAYER_CAPACITY = 2
 HANDSHAKE = str.encode("aok")
+
+logging.basicConfig(level=logging.DEBUG)
 
 class Host:
     def __init__(self, host, port):
@@ -18,51 +24,83 @@ class Host:
             self.__addrs = [None] * PLAYER_CAPACITY
             self.__threads = [None] * PLAYER_CAPACITY
 
-            print(f"Server started on {host}")
+            logging.info(f"Server started on {host}")
 
             # Start player handlers
             for i in range(PLAYER_CAPACITY):
                 self.open_connection(i)
             
             # Wait for enough players to connect
-            print(f"Waiting for Players...", end="\r")
+            logging.info(f"Waiting for Players...")
             while self.__active_conns < PLAYER_CAPACITY:
-                print(f"Connected to {self.__active_conns}/{PLAYER_CAPACITY} Players", end="\r")
+                continue
             
-            print("Connected by " + ' & '.join([(i[0]+":"+str(i[1])) for i in self.__addrs]))
+            logging.info("Connected by " + ' & '.join([(i[0]+":"+str(i[1])) for i in self.__addrs]))
 
         except Exception as e:
             raise e
 
     def open_connection(self, index):
-        self.__conns[index], self.__addrs[index] = self.__socket.accept()
         self.__threads[index] = threading.Thread(target=self.daemon, args=(index,), daemon=True)
         self.__threads[index].start()
-        self.__active_conns += 1
 
+    def close_connection(self, index):
+        try:
+            self.__conns[index].shutdown(socket.SHUT_RDWR)
+            self.__conns[index].close()
+            self.__conns[index] = None
+            self.__active_conns -= 1
+            logging.info(f"Disconnected from player {index + 1}")
+            logging.info(f"Connected to {self.__active_conns}/{PLAYER_CAPACITY} Players")
+        except:
+            logging.warning("Closing a connection failed")
 
     def close(self):
-        self.__socket.close()
+        try:
+            self.__socket.shutdown(socket.SHUT_RDWR)
+        except OSError:
+            # Ignore error if the socket is already closed
+            pass
+        finally:
+            self.__socket.close()
+            self.__socket = None
+            logging.info("Server socket has been closed")
 
     def daemon(self, index):
-        while True:
-            # Listen for move
-            data = self.__conns[index].recv(1024)
-            if not data:
-                break
+        # Establish connection with client
+        self.__conns[index], self.__addrs[index] = self.__socket.accept()
+        self.__active_conns += 1
+        logging.info(f"Connected to {self.__addrs[index][0]}:{str(self.__addrs[index][1])} as player {index + 1}")
+        logging.info(f"Connected to {self.__active_conns}/{PLAYER_CAPACITY} Players")
 
-            # Propgate move to each player
-            for __conn in (__conn for __conn in self.__conns if __conn is not None):
-                __conn.sendall(data)
+        try:
+            while True:
+                # Listen for move
+                data = self.__conns[index].recv(BUFFER_SIZE)
+                if not data:
+                    break
 
-        # Client has closed the connection, close server-side connection and repoen
-        self.__conns[index].close()
-        self.__conns[index] = None
-        self.__active_conns -= 1
+                logging.info(f"Received message from player {index + 1}: {data.decode()}")
+
+                # Propgate move to each player
+                for __conn in (__conn for __conn in self.__conns if __conn is not None):
+                    __conn.sendall(data)
+        except ConnectionResetError as e:
+            logging.warning(f"Client {self.__addrs[index][0]}:{str(self.__addrs[index][1])} forcefully disconnected")
+
+        # Client has closed the connection, close server-side connection
+        self.close_connection(index)
+
+        # Reopen the connection
         self.open_connection(index)
 
 server = Host(HOST, HOST_PORT)
 
-_ = input("Server is active! Press enter to Deactivate server...")
+logging.info("Server is active! Type \"end\" to Deactivate server...")
+
+while True:
+    user_input = input()
+    if user_input == "end":
+        break
 
 server.close()
